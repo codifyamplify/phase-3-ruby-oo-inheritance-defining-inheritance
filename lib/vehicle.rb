e8b622d97c5b6c76bf5d4978a06398ee8a34d1fa
David Whitney <codifyamplify@gmail.com>
2022-12-29 18:29:58 -0500

    commit tree ac0ec8b33cba5766c5ef970d49b5148df7846d0e

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,44 @@
+# this Vehicle class will be a superclass, or parent class. this will hold attributes and behaviors that the children classes will be built to inherit
+require 'pry'
 class Vehicle
+    # macros
+    attr_accessor :wheel_size, :wheel_number
+    # initialize
+    def initialize(wheel_size, wheel_number)
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
+    # methods
+    def go
+        "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank
+        "filling up!"
+    end
 
 end
+
+
+# class Vehicle
+
+#     attr_accessor :wheel_size, :wheel_number
+  
+#     def initialize(wheel_size, wheel_number)
+#       @wheel_size = wheel_size
+#       @wheel_number = wheel_number
+#     end
+  
+#     def go
+#       "vrrrrrrrooom!"
+#     end
+  
+#     def fill_up_tank
+#       "filling up!"
+#     end
+  
+#   end
+
+
+  # lemon = Vehicle.new('small', 4)
+# puts lemon.wheel_size
